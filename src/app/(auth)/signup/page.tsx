df8a601625355be6731780ed175bdf875ea9d359
Hasan Toker <75892548+tokerto7001@users.noreply.github.com>
2024-06-29 15:06:53 +0300

    feat: add signin feat for front end

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import FitnessImage from "@/../public/fitness.webp";
 import AuthenticationForm from "@/components/auth/authentication-form";
 
-export default function SignIn() {
+export default function SignUp() {
   return (
     <div className="w-screen h-screen flex">
       <div className="w-2/5 hidden md:block">
@@ -13,7 +13,7 @@ export default function SignIn() {
         />
       </div>
       <div className="md:w-3/5 w-screen flex justify-center items-center">
-        <AuthenticationForm authType="Login" />
+        <AuthenticationForm authType="Register" />
       </div>
     </div>
   );
--- src/components/auth/authentication-form.tsx
+++ src/components/auth/authentication-form.tsx
@@ -1,96 +1,130 @@
-'use client';
+"use client";
 
 import { useForm } from "react-hook-form";
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "../ui/form"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
 import { z } from "zod";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
+import Link from "next/link";
 
 interface AuthenticationFormProps {
-    authenticationText: 'Login' | 'Register'
+  authType: "Login" | "Register";
 }
 
 const formSchema = z.object({
-    email: z.string({required_error: 'Email Required'}).email({message: 'Please provide a valid email'}),
-    password: z.string({required_error: 'Password Required'}).min(8, {message: 'Password must be more than 8 characters'})
-})
+  email: z
+    .string({ required_error: "Email Required" })
+    .email({ message: "Please provide a valid email" }),
+  password: z
+    .string({ required_error: "Password Required" })
+    .min(8, { message: "Password must be more than 8 characters" }),
+});
 
-export default function AuthenticationForm({authenticationText}: AuthenticationFormProps){
+export default function AuthenticationForm({
+  authType,
+}: AuthenticationFormProps) {
+  const form = useForm<z.infer<typeof formSchema>>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+  const { handleSubmit, control, setError, reset, formState: {isSubmitting} } = form;
 
-    const form = useForm<z.infer<typeof formSchema>>({
-        defaultValues: {
-            email: '',
-            password: ''
-        }
-    });
-    const { handleSubmit, control, setError, reset } = form;
-
-    function onSubmit(formValues: z.infer<typeof formSchema>){
-        const parseResult = formSchema.safeParse(formValues);
-        if(parseResult.error) {
-            const fieldErrors = parseResult.error.flatten().fieldErrors;
-            Object.keys(fieldErrors).map((key) => {
-                setError(key as keyof z.infer<typeof formSchema>, { type: 'custom', message: (fieldErrors[key as keyof z.infer<typeof formSchema>] as string[]).toString()});
-            });
-            reset({email: '', password: ''}, {keepErrors: true});
-        }
-        
+  async function onSubmit(formValues: z.infer<typeof formSchema>) {
+    const parseResult = formSchema.safeParse(formValues);
+    if (parseResult.error) {
+      const fieldErrors = parseResult.error.flatten().fieldErrors;
+      Object.keys(fieldErrors).map((key) => {
+        setError(key as keyof z.infer<typeof formSchema>, {
+          type: "custom",
+          message: (
+            fieldErrors[key as keyof z.infer<typeof formSchema>] as string[]
+          ).toString(),
+        });
+      });
+      reset({ email: "", password: "" }, { keepErrors: true });
     }
+  }
 
-    return (
-        <div className="h-[80%] w-[50%] flex-col">
-            <div className="pt-5 flex justify-center h-1/5">
-                <div className="w-36">
-                <h3 className='text-5xl text-center m-3'>{authenticationText}</h3>
-                <hr />
-                </div>
-
-            </div>
-            <div className="h-4/5">
-                <Form {...form}>
-                    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-8">
-                        <FormField
-                        control={control}
-                        name="email"
-                        render={({field}) => (
-                            <FormItem>
-                                <FormLabel>Email</FormLabel>
-                                <FormControl>
-                                    <Input
-                                    className="border-gray-300 focus:border-black border-2 h-12"
-                                    placeholder="email"
-                                    {...field}
-                                    />
-                                </FormControl>
-                                <FormMessage
-                                className="mt-4"
-                                />
-                                </FormItem>
-                        )}
-                        />
-                        <FormField
-                        control={control}
-                        name="password"
-                        render={({field}) => (
-                            <FormItem>
-                                <FormLabel>Password</FormLabel>
-                                <FormControl>
-                                    <Input
-                                    className="border-gray-300 focus:border-black border-2 h-12"
-                                    placeholder="password"
-                                    {...field}
-                                    />
-                                </FormControl>
-                                <FormMessage
-                                className="mt-4"
-                                />
-                                </FormItem>
-                        )}
-                        />
-                        <Button type="submit">Submit</Button>
-                    </form>
-                </Form>
-            </div>
+  return (
+    <div className="h-[80%] w-[50%] flex-col">
+      <div className="pt-5 flex justify-center h-1/5">
+        <div className="w-full">
+          <h3 className="text-5xl text-center mb-7">{authType}</h3>
+          <hr />
         </div>
-    )
+      </div>
+      <div className="h-4/5">
+        <Form {...form}>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-col gap-8"
+          >
+            <FormField
+              control={control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      className="border-gray-300 focus:border-black border-2 h-12"
+                      placeholder="email"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage className="mt-4" />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      className="border-gray-300 focus:border-black border-2 h-12"
+                      placeholder="password"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage className="mt-4" />
+                </FormItem>
+              )}
+            />
+            <Button className="h-12 bg-black" type="submit" disabled={isSubmitting}>
+              {authType}
+            </Button>
+          </form>
+        </Form>
+        <div className="mt-5 text-center">
+          {authType === "Login" ? (
+            <p className="text-gray-500">
+              Don't have an account?
+              <Link className="text-black font-semibold ml-1" href="/signup">
+                Register for free
+              </Link>
+            </p>
+          ) : (
+            <p className="text-gray-500">
+              Already have an account?
+              <Link className="text-black font-semibold ml-1" href="/signin">
+                Login here
+              </Link>
+            </p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
